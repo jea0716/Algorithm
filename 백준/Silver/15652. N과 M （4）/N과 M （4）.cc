bb19a5f44efd70effe5ab5c727ac75ee18b82f7c
동재 <59719500+jea0716@users.noreply.github.com>
2023-02-05 19:42:18 +0900

    [Silver III] Title: N과 M (4), Time: 416 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int arr[8];
+bool visited[8];
+int n, m, cnt;
+vector <int> v;
+
+
+void print()
+{
+    bool check = true;
+    for(int i=0; i<v.size()-1; i++)
+    {
+        if(v[i] > v[i+1]) check = false;
+    }
+    if(check == true)
+    {
+        for(int i=0; i<v.size(); i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << endl;
+    }
+    return ;
+}
+
+void dfs(int num, int cnt)
+{
+    if(cnt == m)
+    {
+        print();
+        return ;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        visited[i] = true;
+        v.push_back(arr[i]);
+        dfs(i, cnt + 1);
+        v.pop_back();
+        visited[i] = false; 
+    }
+}
+
+int main()
+{   
+    cin >> n >> m;
+    for(int i=0; i<n; i++)
+    {
+        arr[i] = i + 1;
+        visited[i] = false;
+    }
+
+    dfs(1, 0);
+
+    return 0;
+}
