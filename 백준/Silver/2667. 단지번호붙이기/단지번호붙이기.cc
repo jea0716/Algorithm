635b5e08c7b2e332b72431a9b374e88ace08b0ea
동재 <59719500+jea0716@users.noreply.github.com>
2024-01-17 13:20:22 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,75 +1,68 @@
 #include <iostream>
-#include <string>
 #include <queue>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int arr[30][30], check[30][30], path[30][30] = {1, };
-vector <int> v;
+struct coordinate {
+    int X, Y;
+};
 
-void bfs(int n, int m)
-{
-    queue <int> q[2];
-    q[0].push(n); q[1].push(m);
-    check[n][m] = 1;
-    int tmp = 1;
-    while(!q[0].empty())
-    {
-        int x = q[0].front(), y = q[1].front();
-        q[0].pop(); q[1].pop();
-        if(arr[x-1][y] == 1 && check[x-1][y] == 0)
-        {
-            check[x-1][y] = 1;
-            path[x-1][y] = path[x][y] + 1; tmp++;
-            q[0].push(x-1); q[1].push(y);
-        }
-        if(arr[x+1][y] == 1 && check[x+1][y] == 0)
-        {
-            check[x+1][y] = 1;
-            path[x+1][y] = path[x][y] + 1; tmp++;
-            q[0].push(x+1); q[1].push(y);
-        }
-        if(arr[x][y-1] == 1 && check[x][y-1] == 0)
-        {
-            check[x][y-1] = 1;
-            path[x][y-1] = path[x][y] + 1; tmp++;
-            q[0].push(x); q[1].push(y-1);
-        }
-        if(arr[x][y+1] == 1 && check[x][y+1] == 0)
-        {
-            check[x][y+1] = 1;
-            path[x][y+1] = path[x][y] + 1; tmp++;
-            q[0].push(x); q[1].push(y+1);
+int N;
+char map[26][26];
+int visited[26][26];
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+int BFS_SIZE;
+
+int BFS(int x, int y) {
+    queue <coordinate> q;
+    q.push({ x, y });
+    visited[x][y] = 1;
+    BFS_SIZE = 1;
+
+    while(!q.empty()) {
+        int x = q.front().X;
+        int y = q.front().Y;
+        q.pop();
+
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+            if(map[nx][ny] == '0' || visited[nx][ny] == 1) continue;
+            visited[nx][ny] = 1;
+            BFS_SIZE++;
+            q.push({ nx, ny });
         }
     }
-    v.push_back(tmp);
+
+    return BFS_SIZE;
 }
 
-int main()
-{
-    int n; cin >> n;
+int main() {
+    cin >> N;
+    vector <int> v;
 
-    for(int i=0; i<n; i++)
-    {
-        string s; cin >> s;
-        for(int j=0; j<n; j++)
-        {
-            if(s[j] == '0') arr[i+1][j+1] = 0;
-            else arr[i+1][j+1] = 1;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            cin >> map[i][j];
         }
     }
 
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=1; j<=n; j++)
-        {
-            if(arr[i][j] == 1 && check[i][j] == 0) bfs(i, j);
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            if(visited[i][j] == 1) continue;
+            if(map[i][j] == '0') continue;
+            v.push_back(BFS(i, j));
         }
     }
 
-    sort(v.begin(), v.begin()+v.size());
-    cout << v.size() << endl;
-    for(int i=0; i<v.size(); i++) cout << v[i] << endl;
+    sort(v.begin(), v.end());
+    cout << v.size() << "\n";
+    for(int i=0; i<v.size(); i++) cout << v[i] << "\n";
+
+    return 0;
 }
