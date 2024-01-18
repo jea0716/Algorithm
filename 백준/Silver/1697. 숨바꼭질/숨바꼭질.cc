d7908122d7dfb5f09047b18f26233a11a08af2c4
동재 <59719500+jea0716@users.noreply.github.com>
2024-01-18 11:36:50 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -4,48 +4,45 @@
 
 using namespace std;
 
-int n, m;
-int check[100001];
-int path[100001];
+struct node {
+    int X, Count;
+};
 
-queue <int> q;
+int to, from;
+int visited[100001];
 
-void bfs(int s)
-{
-    path[s] = 0;
-    q.push(s);
-    check[s] = 1;
+void BFS(int n) {
+    queue <node> q;
+    q.push({ n, 0 });
+    visited[n] = 1;
 
-    while(!q.empty())
-    {
-        int x = q.front();
-        if(x == m) break;
+    while(!q.empty()) {
+        int n = q.front().X;
+        int count = q.front().Count;
         q.pop();
 
-        if(check[x+1] == 0 && x+1 >= 0 && x+1 < 100001)
-        {
-            check[x+1] = 1;
-            path[x+1] = path[x] + 1;
-            q.push(x+1);
+        if(n + 1 <= 100000 && visited[n + 1] > count + 1) {
+            visited[n + 1] = count + 1;
+            q.push({ n + 1, count + 1 });
         }
-        if(check[x-1] == 0 && x-1 >= 0 && x-1 < 100001)
-        {
-            check[x-1] = 1;
-            path[x-1] = path[x] + 1;
-            q.push(x-1);
+        if(n - 1 >= 0 && visited[n - 1] > count + 1) {
+            visited[n - 1] = count + 1;
+            q.push({ n - 1, count + 1});
         }
-        if(check[x*2] == 0 && x*2 >= 0 && x*2 < 100001)
-        {
-            check[x*2] = 1;
-            path[x*2] = path[x] + 1;
-            q.push(x*2);
+        if(n * 2 <= 100000 && visited[n * 2] > count + 1) {
+            visited[n * 2] = count + 1;
+            q.push({ n * 2, count + 1 });
         }
     }
 }
 
-int main()
-{
-    cin >> n >> m;
-    bfs(n);
-    cout << path[m] << '\n';
+int main() {
+    cin >> to >> from;
+
+    for(int i=0; i<=100000; i++) visited[i] = 9999999;
+
+    BFS(to);
+
+    if(to == from) cout << 0 << endl;
+    else cout << visited[from] << "\n";
 }
