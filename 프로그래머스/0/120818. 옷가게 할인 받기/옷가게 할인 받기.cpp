8f7e41f2d9bbac280c410fb19bfc438582c79f8d
동재 <59719500+jea0716@users.noreply.github.com>
2024-02-25 12:26:44 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    if(price >= 500000) answer = price * 0.8;
+    else if(price >= 300000) answer = price * 0.9;
+    else if(price >= 100000) answer = price * 0.95;
+    else answer = price;
+    return answer;
+}
