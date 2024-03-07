ab03cbbac79393a9cfceea916d35589a816640e6
동재 <59719500+jea0716@users.noreply.github.com>
2024-03-07 16:42:05 +0900

    [level 0] Title: [PCCE 기출문제] 2번 / 피타고라스의 정리, Time: 2.23 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/250132. ［PCCE 기출문제］ 2번 ／ 피타고라스의 정리/［PCCE 기출문제］ 2번 ／ 피타고라스의 정리.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int c;
+    cin >> a >> c;
+    
+    int b_square = c*c - a*a;
+    cout << b_square << endl;
+    return 0;
+}
