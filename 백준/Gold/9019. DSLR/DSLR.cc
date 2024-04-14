810e622949de62c7970a1146cf9908f16082fdb7
동재 <59719500+jea0716@users.noreply.github.com>
2024-04-14 17:46:36 +0900

    [Gold IV] Title: DSLR, Time: 4144 ms, Memory: 2316 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -1,118 +1,97 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <string>
-#include <tuple>
+#include <queue>
 
 using namespace std;
 
-struct props{
-    int first;
-    string second;
+struct node {
+    int n;
+    string route;
 };
 
-int N, A, B;
-string S;
-props visited[10000];
+int visited[10000];
 
-void solve(int start_num, string s)
-{
-    queue <props> q;
-    props start;
-    start.first = start_num;
-    start.second = s;
-    q.push(start);
+int L(int n) {
+    int a, b, c, d;
+    d = n % 10;
+    n /= 10;
+    c = n % 10;
+    n /= 10;
+    b = n % 10;
+    n /= 10;
+    a = n % 10;
 
-    while(!q.empty())
-    {
-        int x = q.front().first;
-        q.pop();
-
-        if(x == B) return;
-
-        props tmp;
-
-        if(visited[(2 * x) % 10000].first == 0 && x != 0)
-        {
-            tmp.first = (2 * x) % 10000;
-            tmp.second = visited[x].second;
-            tmp.second.append("D");
-            visited[(2 * x) % 10000].first = 1;
-            if(visited[(2 * x) % 10000].second.length() > tmp.second.length() || visited[(2 * x) % 10000].second == "")
-            {
-                visited[(2 * x) % 10000].second = tmp.second;
-            }
-            q.push(tmp);
-        }
-
-        int minus = x - 1;
-        if(minus == -1) minus = 9999;
-        if(visited[minus].first == 0)
-        {
-            tmp.first = minus;
-            tmp.second = visited[x].second;
-            tmp.second.append("S");
-            visited[minus].first = 1;
-            if(visited[minus].second.length() > tmp.second.length() || visited[minus].second == "")
-            {
-                visited[minus].second = tmp.second;
-            }
-            q.push(tmp);
-        }
-
-        int z = x;
-        int a = z % 10; z /= 10;
-        int b = z % 10; z /= 10;
-        int c = z % 10; z /= 10;
-        int d = z;
-
-        int move_left = c * 1000 + b * 100 + a * 10 + d;
-        int move_right = a * 1000 + d * 100 + c * 10 + b;
-
-        if(visited[move_left].first == 0)
-        {
-            tmp.first = move_left;
-            tmp.second = visited[x].second;
-            tmp.second.append("L");
-            visited[move_left].first = 1;
-            if(visited[move_left].second.length() > tmp.second.length() || visited[move_left].second == "")
-            {
-                visited[move_left].second = tmp.second;
-            }
-            q.push(tmp);
-        }
-
-        if(visited[move_right].first == 0)
-        {
-            tmp.first = move_right;
-            tmp.second = visited[x].second;
-            tmp.second.append("R");
-            visited[move_right].first = 1;
-            if(visited[move_right].second.length() > tmp.second.length() || visited[move_right].second == "")
-            {
-                visited[move_right].second = tmp.second;
-            }
-            q.push(tmp);
-        }
-    }
+    return b * 1000 + c * 100 + d * 10 + a;
 }
 
-int main()
-{
-    cin >> N;
+int R(int n) {
+    int a, b, c, d;
+    d = n % 10;
+    n /= 10;
+    c = n % 10;
+    n /= 10;
+    b = n % 10;
+    n /= 10;
+    a = n % 10;
 
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<10000; j++)
-        {
-            visited[j].first = 0;
-            visited[j].second = "";
+    return d * 1000 + a * 100 + b * 10 + c;
+}
+
+string BFS(int n, int m) {
+    visited[n] = true;
+    queue<node> q;
+    q.push({n, ""});
+
+    while(!q.empty()) {
+        int n = q.front().n;
+        string route = q.front().route;
+        q.pop();
+
+        if(n == m) {
+            return route;
         }
-        cin >> A >> B;
-        S = "";
-        solve(A, S);
-        cout << visited[B].second << endl;
+
+        if(!visited[(n * 2) % 10000]) {
+            q.push({ (n * 2) % 10000, route + 'D' });
+            visited[(n * 2) % 10000] = true;
+        }
+
+        if(!visited[n == 0 ? 9999 : n - 1]) {
+            q.push({ n == 0 ? 9999 : n - 1, route + 'S' });
+            visited[n == 0 ? 9999 : n - 1] = true;
+        }
+
+        int ln = L(n);
+        int rn = R(n);
+
+        if(!visited[ln]) {
+            q.push({ ln, route + 'L' });
+            visited[ln] = true;
+        }
+
+        if(!visited[rn]) {
+            q.push({ rn, route + 'R' });
+            visited[rn] = true;
+        }
     }
+
+    return "";
+}
+
+string solve() {
+    int n, m; cin >> n >> m;
+
+    for(int i=0; i<10000; i++) visited[i] = false;
+
+    return BFS(n, m);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N; cin >> N;
+
+    for(int i=0; i<N; i++) cout <<  solve() << endl;;
 
     return 0;
 }
