85913f13f2c6d03cbe1779a0576cd347e8734bc9
동재 <59719500+jea0716@users.noreply.github.com>
2024-04-16 00:37:05 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -1,62 +1,67 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 
 using namespace std;
 
-int bridge[120], snake[120], path[120], check[120];
+int N, M, to, from, answer=100;
+vector <int> v(101, 0);
+bool visited[101];
 
-queue<int> q;
+struct node {
+    int location, cnt;
+};
 
-void bfs(int n)
-{
-    q.push(n);
-    path[n] = 0;
-    check[n] = 1;
-    while(!q.empty())
-    {
-        int x = q.front();
-        if(x == 100) break;
+void BFS() {
+    queue <node> q;
+    q.push({ 1, 0 });
+    visited[1] = true;
+
+    while(!q.empty()) {
+        int location = q.front().location;
+        int cnt = q.front().cnt;
         q.pop();
-        for(int i=1; i<=6; i++)
-        {
-            if(bridge[x + i] == 0 && snake[x + i] == 0 && check[x + i] == 0)
-            {
-                path[x + i] = path[x] + 1;
-                check[x + i] = 1;
-                q.push(x + i);
+
+        if(location == 100) {
+            answer = answer < cnt ? answer : cnt;
+            return;
+        }
+
+        for(int i=1; i<=6; i++) {
+            if(location + i <= 100 && v[location + i] != 0 && (!visited[v[location + i]] || !visited[location + i])) {
+                q.push({ v[location + i], cnt + 1 });
+                visited[v[location + i]] = true;
+                visited[location + i] = true;
             }
-            else if(bridge[x + i] != 0 && check[bridge[x + i]] == 0)
-            {
-                path[bridge[x + i]] = path[x] + 1;
-                check[bridge[x + i]] = 1;
-                q.push(bridge[x + i]);
-            }
-            else if(snake[x + i] != 0 && check[snake[x + i]] == 0)
-            {
-                path[snake[x + i]] = path[x] + 1;
-                check[snake[x + i]] = 1;
-                q.push(snake[x + i]);
+
+            else {
+                if(location + i <= 100 && !visited[location + i]) {
+                    q.push({ location + i, cnt + 1 });
+                    visited[location + i] = true;
+                }
             }
         }
     }
 }
 
-int main()
-{
-    int N, M; cin >> N >> M;
-    int a, b;
-    for(int i=0; i<N; i++)
-    {
-        cin >> a >> b;
-        bridge[a] = b;
-    }
-    for(int i=0; i<M; i++)
-    {
-        cin >> a >> b;
-        snake[a] = b;
-    }
-    bfs(1);
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
 
+    cin >> N >> M;
 
-    cout << path[100] << endl;
+    for(int i=0; i<N; i++) {
+        cin >> to >> from;
+        v[to] = from;
+    }
+
+    for(int i=0; i<M; i++) {
+        cin >> to >> from;
+        v[to] = from;
+    }
+
+    BFS();
+
+    cout << answer << '\n';
+
+    return 0;
 }
