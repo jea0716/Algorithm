a5d99267106b54ec6fd4396644aaa665e05fb8ee
동재 <59719500+jea0716@users.noreply.github.com>
2024-04-17 14:52:26 +0900

    [Gold V] Title: 내려가기, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -1,39 +1,42 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-    int n; cin >> n;
-    int t1, t2, t3;
-    int m1=0, m2=0, m3=0, n1=0, n2=0, n3=0;
-    int m4, m5, m6, n4, n5, n6;
-    int min, max;
-    for(int i=0; i<n; i++){
-        cin >> t1 >> t2 >> t3;
-        m4=m1; m5=m2; m6=m3;
-        n4=n1; n5=n2; n6=n3;
-        if(m4+t1 > m5+t1) m1 = m4+t1;
-        else m1 = m5+t1;
-        if(m4+t2 >= m5+t2 && m4+t2 >= m6+t2) m2 = m4+t2;
-        else if(m5+t2 >= m4+t2 && m5+t2 >= m6+t2) m2 = m5+t2;
-        else m2 = m6+t2;
-        if(m5+t3 > m6+t3) m3 = m5+t3;
-        else m3 = m6+t3;
+int min_dp[3], max_dp[3];
 
-        if(n4+t1 < n5+t1) n1 = n4+t1;
-        else n1 = n5+t1;
-        if(n4+t2 <= n5+t2 && n4+t2 <= n6+t2) n2 = n4+t2;
-        else if(n5+t2 <= n4+t2 && n5+t2 <= n6+t2) n2 = n5+t2;
-        else n2 = n6+t2;
-        if(n5+t3 < n6+t3) n3 = n5+t3;
-        else n3 = n6+t3;
+int main() {
+    int n; cin >> n;
+
+    int a, b, c, t1, t2, t3;
+    cin >> min_dp[0] >> min_dp[1] >> min_dp[2];
+    max_dp[0] = min_dp[0];
+    max_dp[1] = min_dp[1];
+    max_dp[2] = min_dp[2];
+
+    for(int i=1; i<n; i++) {
+        cin >> a >> b >> c;
+        t1 = a + min(min_dp[0], min_dp[1]);
+        t2 = b + min(min_dp[0], min(min_dp[1], min_dp[2]));
+        t3 = c + min(min_dp[1], min_dp[2]);
+
+        min_dp[0] = t1;
+        min_dp[1] = t2;
+        min_dp[2] = t3;
+
+        t1 = a + max(max_dp[0], max_dp[1]);
+        t2 = b + max(max_dp[0], max(max_dp[1], max_dp[2]));
+        t3 = c + max(max_dp[1], max_dp[2]);
+
+        max_dp[0] = t1;
+        max_dp[1] = t2;
+        max_dp[2] = t3;
     }
-    if(m1 >= m2 && m1 >= m3) max = m1;
-    else if(m2 >= m1 && m2 >= m3) max = m2;
-    else max = m3;
-    if(n1 <= n2 && n1 <= n3) min = n1;
-    else if(n2 <= n3 && n2 <= n1) min = n2;
-    else min = n3;
-    cout << max << " " << min << endl;
+
+    int min_num = min(min_dp[0], min(min_dp[1], min_dp[2]));
+    int max_num = max(max_dp[0], max(max_dp[1], max_dp[2]));
+
+    cout << max_num << " " << min_num << "\n";
+
     return 0;
 }
