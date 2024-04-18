4c593f0c440ac38b547eef5e2563e652502336af
동재 <59719500+jea0716@users.noreply.github.com>
2024-04-18 16:02:12 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 3.85 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long perm(int n) // n! 값 구하기
+{
+    if(n == 0) return 1;
+    return n * perm(n - 1);
+}
+
+void func(vector<int>& v, vector<int>& answer, long long& k)
+{
+    if (v.size() == 1) {
+        answer.push_back(v[0]);
+        return;
+    }
+
+    long long p = perm(v.size() - 1); 
+    for (int i = 1; i <= v.size(); ++i) {
+        if (i * p >= k) {
+            answer.push_back(v[i - 1]); // i번째기 때문에 인덱스론 i-1
+            v.erase(v.begin() + i - 1);
+            k = k - (i - 1) * p;
+            func(v, answer, k);
+        }
+    }
+}
+
+vector<int> solution(int n, long long k) {
+    vector<int> answer;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        v[i] = i + 1; 
+
+    func(v, answer, k);
+    
+    return answer;
+}
