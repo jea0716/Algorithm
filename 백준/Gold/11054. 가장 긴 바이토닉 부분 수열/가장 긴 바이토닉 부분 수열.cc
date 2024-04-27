08d465d877e08fd1c581d40c19c3fc2dce9b971f
동재 <59719500+jea0716@users.noreply.github.com>
2024-04-27 15:00:26 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,43 +1,45 @@
 #include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-int dp_up[1001];
-int dp_down[1001];
+int N, answer = 1;
+int arr[1001];
+int dp_max[1001], dp_min[1001];
 
 int main() {
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
+    cin >> N;
+    for(int i=1; i<=N; i++) cin >> arr[i];
 
-    int n; cin >> n;
-    int arr[n];
-    for(int i=0; i<n; i++) cin >> arr[i];
-
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<i; j++) {
-            if(arr[i] > arr[j] && dp_up[j] >= dp_up[i]) {
-                dp_up[i] = dp_up[j] + 1;
-            }
-        }   
-    }
-
-    for(int i=n-1; i>=0; i--) {
-        for(int j=n-1; j>=i; j--) {
-            if(arr[i] > arr[j] && dp_down[j] >= dp_down[i]) {
-                dp_down[i] = dp_down[j] + 1;
+    dp_max[1] = 1;
+    for(int i=2; i<=N; i++) {
+        int cnt = 0;
+        for(int j=1; j<i; j++) {
+            if(arr[i] > arr[j]) {
+                if(dp_max[j] > cnt) cnt = dp_max[j];
             }
         }
+        dp_max[i] = cnt + 1;
     }
 
-    int answer = 0;
-
-    for(int i=0; i<n; i++) {
-        if(answer < dp_up[i] + dp_down[i] + 1) {
-            answer = dp_up[i] + dp_down[i] + 1;
+    for(int i=N; i>0; i--) {
+        int cnt = 0;
+        for(int j=N; j>i; j--) {
+            if(arr[i] > arr[j]) {
+                if(dp_min[j] > cnt) cnt = dp_min[j];
+            }
         }
+        dp_min[i] = cnt + 1;
     }
 
-    cout << answer << "\n";
+    for(int i=1; i<=N; i++) {
+        answer = answer > dp_max[i] + dp_min[i] - 1 ? answer : dp_max[i] + dp_min[i] - 1;
+    }
+
+    cout << answer << endl;
 
     return 0;
 }
