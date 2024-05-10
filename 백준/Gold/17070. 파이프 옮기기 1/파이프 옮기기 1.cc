9aa28846ba1cfbca791b733f080a98317ab1b887
동재 <59719500+jea0716@users.noreply.github.com>
2024-05-10 13:19:58 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,73 +1,42 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
 int N;
 int map[16][16];
-int dp[16][16];
+int dp[16][16][3];
 
-struct shape{
-    int x, y;
-    bool a, b, c;
-};
+int main() {
+    cin >> N;
 
-void solve(int start_x, int start_y, bool a, bool b, bool c)
-{
-    queue <shape> q;
-    shape start = {start_x, start_y, a, b, c};
-    q.push(start);
-
-    while(!q.empty())
-    {
-        int xx = q.front().x;
-        int yy = q.front().y;
-        bool aa = q.front().a;
-        bool bb = q.front().b;
-        bool cc = q.front().c;
-        q.pop();
-
-        if(xx + 1 < N && map[xx+1][yy] == 0 && cc == true)
-        {
-            dp[xx+1][yy] += 1;
-            shape tmp = {xx+1, yy, false, true, true};
-            q.push(tmp);
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<N; j++) {
+            cin >> map[i][j];
         }
+    }
 
-        if(yy + 1 < N && map[xx][yy+1] == 0 && aa == true)
-        {
-            dp[xx][yy+1] += 1;
-            shape tmp = {xx, yy+1, true, true, false};
-            q.push(tmp);
-        }
+    dp[0][1][0] = 1;
+    for(int i=2; i<N; i++) {
+        if(map[0][i] == 0) dp[0][i][0] = dp[0][i-1][0];
+    }
 
-        if(xx + 1 < N && yy + 1 < N && bb == true)
-        {
-            if(map[xx+1][yy] == 0 && map[xx+1][yy+1] == 0 && map[xx][yy+1] == 0)
-            {
-                dp[xx+1][yy+1] += 1;
-                shape tmp = {xx+1, yy+1, true, true, true};
-                q.push(tmp);
+    for(int i=1; i<N; i++) {
+        for(int j=2; j<N; j++) {
+            if(map[i][j] == 0) {
+                dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1];
+                dp[i][j][2] = dp[i-1][j][1] + dp[i-1][j][2];
+                if(map[i-1][j] == 0 && map[i][j-1] == 0) {
+                    dp[i][j][1] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+                }
             }
         }
     }
-}
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    cin >> N;
-
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++) cin >> map[i][j];
-    }
-
-    solve(0, 1, true, true, false);
-    cout << dp[N-1][N-1] << "\n";
+    cout << dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2] << endl;
 
     return 0;
 }
