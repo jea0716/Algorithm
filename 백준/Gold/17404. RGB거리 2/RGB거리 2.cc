a6771262d4cfc1e0f3db44c309a1e6830684431e
동재 <59719500+jea0716@users.noreply.github.com>
2024-06-02 19:07:38 +0900

    [Gold IV] Title: RGB거리 2, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.cc
@@ -1,76 +1,57 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int N;
-int f_R, f_G, f_B;
-int l_R, l_G, l_B;
-int R, G, B;
-int A1[3], A2[3], A3[3];
-int t1, t2, t3;
-int answer;
+const int INF = 1e9;
+int N, answer = INF;
+int arr[1001][3];
+int dp[1001][3];
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
+int main() {
     cin >> N;
-
-    cin >> f_R >> f_G >> f_B;
-
-    A1[0] = f_R;        A1[1] = 99999999;   A1[2] = 99999999;
-    A2[0] = 99999999;   A2[1] = f_G;    A2[2] = 99999999;
-    A3[0] = 99999999;   A3[1] = 99999999;   A3[2] = f_B;
-
-    for (int i = 1; i < N - 1; i++)
-    {
+    
+    int R, G, B;
+    for(int i=1; i<=N; i++) {
         cin >> R >> G >> B;
-
-        t1 = A1[0], t2 = A1[1], t3 = A1[2];
-
-        A1[0] = min(t2, t3) + R;
-        A1[1] = min(t1, t3) + G;
-        A1[2] = min(t1, t2) + B;
-
-        t1 = A2[0], t2 = A2[1], t3 = A2[2];
-
-        A2[0] = min(t2, t3) + R;
-        A2[1] = min(t1, t3) + G;
-        A2[2] = min(t1, t2) + B;
-
-        t1 = A3[0], t2 = A3[1], t3 = A3[2];
-
-        A3[0] = min(t2, t3) + R;
-        A3[1] = min(t1, t3) + G;
-        A3[2] = min(t1, t2) + B;
+        arr[i][0] = R;
+        arr[i][1] = G;
+        arr[i][2] = B;
     }
 
-    cin >> l_R >> l_G >> l_B;
+    dp[1][0] = arr[1][0];
+	dp[1][1] = dp[1][2] = INF;
 
-    t1 = A1[0], t2 = A1[1], t3 = A1[2];
+	for (int i = 2; i <= N; i++) {
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+	}
+	answer = min(answer, min(dp[N][1], dp[N][2]));
 
-    A1[1] = min(t1, t3) + l_G;
-    A1[2] = min(t1, t2) + l_B;
+    dp[1][1] = arr[1][1];
+	dp[1][0] = dp[1][2] = INF;
 
-    answer = min(A1[1], A1[2]);
+	for (int i = 2; i <= N; i++) {
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+	}
+	answer = min(answer, min(dp[N][0], dp[N][2]));
+    
+	dp[1][2] = arr[1][2];
+	dp[1][0] = dp[1][1] = INF;
 
-    t1 = A2[0], t2 = A2[1], t3 = A2[2];
+	for (int i = 2; i <= N; i++) {
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+	}
+	answer = min(answer, min(dp[N][0], dp[N][1]));
 
-    A2[0] = min(t2, t3) + l_R;
-    A2[2] = min(t1, t2) + l_B;
-
-    answer = min(answer, min(A2[0], A2[2]));
-
-    t1 = A3[0], t2 = A3[1], t3 = A3[2];
-
-    A3[0] = min(t2, t3) + l_R;
-    A3[1] = min(t1, t3) + l_G;
-
-    answer = min(answer, min(A3[0], A3[1]));
-
-    cout << answer << "\n";
+	cout << answer << '\n';
 
     return 0;
 }
