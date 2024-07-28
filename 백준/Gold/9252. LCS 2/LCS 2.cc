0fc3b71a6577139d8d6d58fa3c34ecef1e9ea2ee
동재 <59719500+jea0716@users.noreply.github.com>
2024-07-28 19:51:57 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 6008 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,50 +1,52 @@
 #include <iostream>
-#include <string>
+#include <vector>
 #include <algorithm>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
-string a, b, answer;
+string A, B;
+vector<vector<int>> dp;
 
-int dp[1001][1001];
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    
+    cin >> A >> B;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie();
+    dp.resize(A.size() + 1, vector<int>(B.size() + 1));
 
-    string tmp1, tmp2;
-    cin >> tmp1 >> tmp2;
-    a = ' ' + tmp1; b = ' ' + tmp2;
-
-    for(int i=1; i<b.size(); i++)
-    {
-        for(int j=1; j<a.size(); j++)
-        {
-            if(a[j] == b[i]) dp[i][j] = dp[i-1][j-1] + 1;
-            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+    for(int i=1; i<=A.length(); i++) {
+        for(int j=1; j<=B.length(); j++) {
+            if(A[i - 1] == B[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j],  dp[i][j - 1]);
         }
     }
 
-    int x = a.size() - 1, y = b.size() - 1;
+    cout << dp[A.length()][B.length()] << '\n';
 
-    while(dp[y][x])
-    {
-        if(dp[y][x] == dp[y-1][x]) y -= 1;
-        else if(dp[y][x] == dp[y][x-1]) x -= 1;
-        else
-        {
-            answer += a[x];
-            y -= 1; x -= 1;
+    int i = A.length();
+    int j = B.length();
+    vector<char> answer;
+
+    while(i > 0 && j > 0) {
+        if(dp[i][j] == dp[i - 1][j]) {
+            i--;
+        } 
+        else if(dp[i][j] == dp[i][j - 1]) {
+            j--;
+        }
+        else {
+            answer.push_back(A[i - 1]);
+            i--;
+            j--;
         }
     }
 
-    cout << dp[b.size()-1][a.size()-1] << "\n";
-    if(answer.size() > 0) 
-    {
-        reverse(answer.begin(), answer.end());
-        cout << answer << "\n";
+    for(int i=answer.size() - 1; i>=0; i--) {
+        cout << answer[i];
     }
+    cout << '\n';
 
     return 0;
 }
