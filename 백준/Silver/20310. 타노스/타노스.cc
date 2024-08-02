e4838108d283953fd430944883bbe4125d5eea26
동재 <59719500+jea0716@users.noreply.github.com>
2024-08-02 13:50:54 +0900

    [Silver III] Title: 타노스, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.cc
+++ 백준/Silver/20310. 타노스/타노스.cc
@@ -13,6 +13,10 @@ int main() {
     string S;
     cin >> S;
 
+    vector<char> v;
+    for(int i=0; i<S.length(); i++) v.push_back(S[i]);
+    vector<bool> vis(v.size(), 0);
+
     int A = 0, B = 0;
     for(int i=0; i<S.length(); i++) {
         if(S[i] == '0') A++;
@@ -21,8 +25,23 @@ int main() {
 
     A /= 2, B /= 2;
 
-    for(int i=0; i<A; i++) cout << '0';
-    for(int i=0; i<B; i++) cout << '1';
+    for(int i=0; i<v.size(); i++) {
+        if(B > 0 && v[i] == '1') {
+            vis[i] = 1;
+            B--;
+        }
+    }
+
+    for(int i=v.size() - 1; i>=0; i--) {
+        if(A > 0 && v[i] == '0') {
+            vis[i] = 1;
+            A--;
+        }
+    }
+
+    for(int i=0; i<v.size(); i++) {
+        if(vis[i] == 0) cout << v[i];
+    }
     cout << '\n';
 
 
