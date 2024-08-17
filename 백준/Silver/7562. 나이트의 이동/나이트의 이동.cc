2889f8305ecfa68e0eee4921fb479cfa449f8e46
동재 <59719500+jea0716@users.noreply.github.com>
2024-08-17 13:26:39 +0900

    [Silver I] Title: 나이트의 이동, Time: 24 ms, Memory: 2240 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,78 +1,70 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-#include <tuple>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-int map[301][301];
-int visited[301][301];
-int N, answer;
+int K,N;
+int to_x, to_y;
+int from_x, from_y;
+bool visited[301][301];
 
-int col[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
-int row[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+int dx[] = { 1, 1, -1, -1, 2, 2, -2, -2 };
+int dy[] = { 2, -2, 2, -2, 1, -1, 1, -1 };
 
-void bfs(int n, int m, int x, int y, int cnt) {
-    if(n == x && m == y) {
-        answer = cnt;
-        return;
+void clear(int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            visited[i][j] = 0;
+        }
     }
+}
 
-    queue <tuple<int, int, int> > q;
-    q.push(make_tuple(n, m, cnt));
-    visited[n][m] = 1;
+void solve() {
+    cin >> N;
+    clear(N);
+    cin >> to_x >> to_y;
+    cin >> from_x >> from_y;
+
+    queue<pair<int, pair<int, int>>> q;
+    q.push({ 0, { to_x, to_y } });
+    visited[to_x][to_y] = 1;
 
     while(!q.empty()) {
-        int a = get<0>(q.front());
-        int b = get<1>(q.front());
-        int z = get<2>(q.front());
-        if(a == x && b == y) {
-            answer = z;
-            return;
-        }
+        int cnt = q.front().first;
+        int cx = q.front().second.first;
+        int cy = q.front().second.second;
         q.pop();
+
+        if(cx == from_x && cy == from_y) {
+            cout << cnt << '\n';
+            return ;
+        }
+
         for(int i=0; i<8; i++) {
-            if(a+col[i] >= 0 && a+col[i] < N && b+row[i] >= 0 && b+row[i] < N) {
-                if(visited[a+col[i]][b+row[i]] == 0) {
-                    q.push(make_tuple(a+col[i], b+row[i], z+1));
-                    visited[a+col[i]][b+row[i]] = 1;
-                }
-            } 
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+            if(nx < 0 || nx >= N) continue;
+            if(ny < 0 || ny >= N) continue;
+            if(visited[nx][ny]) continue;
+
+            q.push({ cnt +1 , { nx, ny } });
+            visited[nx][ny] = 1;
         }
     }
 
-    return;
-}
-
-void reset(int n) {
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) visited[i][j] = 0;
-    }
-
-    return;
-}
-
-void solve(int n) {
-    int from_x, from_y, to_x, to_y;
-    cin >> from_x >> from_y >> to_x >> to_y;
-
-    bfs(from_x, from_y, to_x, to_y, 0);
-
-    return;
+    return ;
 }
 
 int main() {
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false); cin.tie(NULL);
 
-    int num; cin >> num;
+    cin >> K;
 
-    for(int k=0; k<num; k++) {
-        cin >> N;
-        answer = 0;
-        reset(N);
-        solve(N);
-        cout << answer << "\n";
+    while(K--) {
+        solve();
     }
 
     return 0;
