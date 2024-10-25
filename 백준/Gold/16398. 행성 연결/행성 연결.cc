fe788d91051c24389cba44e1386715a9c29ae191
동재 <59719500+jea0716@users.noreply.github.com>
2024-10-25 15:36:00 +0900

    [Gold IV] Title: 행성 연결, Time: 296 ms, Memory: 41176 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int N, tmp;
+long long answer;
+int set[1001];
+vector<vector<int>> v;
+
+int getParent(int n) {
+    if(set[n] == n) return n;
+    return set[n] = getParent(set[n]);
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    cin >> N;
+    v.reserve(N * N + 1);
+
+    for(int i=1; i<=N; i++) set[i] = i;
+
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=N; j++) {
+            cin >> tmp;
+            if(i == j) continue;
+            if(i < j && i != j) v.push_back({ tmp, i, j });
+        }
+    }
+
+    sort(v.begin(), v.end());
+
+    for(int i=0; i<v.size(); i++) {
+        int s = getParent(v[i][1]);
+        int e = getParent(v[i][2]);
+        int cost = v[i][0];
+
+        if(s != e) {
+            answer += cost;
+            set[s] = e;
+        }
+    }
+
+    cout << answer << '\n';
+
+    return 0;
+}   
