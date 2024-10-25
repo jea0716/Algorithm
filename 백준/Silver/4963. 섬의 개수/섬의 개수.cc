d0414382954a1c1747b04563a7cb155b265104e8
동재 <59719500+jea0716@users.noreply.github.com>
2024-10-25 14:47:29 +0900

    [Silver II] Title: 섬의 개수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.cc
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -1,54 +1,68 @@
 #include <iostream>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
-int map[52][52];
-int visited[52][52];
-int W, H;
+int N, M, cnt;
+int map[51][51];
+bool visited[51][51];
 
-int col[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-int row[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+int dx[] = { 1, 1, 1, 0, 0, -1, -1, -1 };
+int dy[] = { 1, 0, -1, 1, -1, 1, 0, -1 };
 
-void bfs(int n, int m) {
-    visited[n][m] = 1;
-    
-    for(int i=0; i<8; i++) {
-        if(map[n+col[i]][m+row[i]] == 1 && visited[n+col[i]][m+row[i]] == 0) {
-            bfs(n+col[i], m+row[i]);
-        } 
+void BFS(int i, int j) {
+    queue<pair<int, int>> q;
+    q.push({ i, j });
+    visited[i][j] = true;
+
+    while(!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for(int i=0; i<8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= M) continue;
+            if(ny < 0 || ny >= N) continue;
+            if(visited[nx][ny]) continue;
+            if(map[nx][ny] == 0) continue;
+
+            q.push({ nx, ny });
+            visited[nx][ny] = true;
+        }
     }
-
-    return;
 }
 
 int main() {
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false); cin.tie(NULL);
 
-    cin >> W >> H;
+    cin >> N >> M;
 
-    while(W != 0 && H != 0) {
-        for(int i=1; i<=H; i++) {
-            for(int j=1; j<=W; j++) {
+    while(N != 0 || M != 0) {
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<N; j++) {
                 cin >> map[i][j];
-                visited[i][j] = 0;
+                visited[i][j] = false;
             }
         }
 
-        int answer = 0;
-
-        for(int i=1; i<=H; i++) {
-            for(int j=1; j<=W; j++) {
-                if(map[i][j] == 1 && visited[i][j] == 0) {
-                    answer++;
-                    bfs(i, j);
+        int cnt = 0;
+        for(int i=0; i<M; i++) {
+            for(int j=0; j<N; j++) {
+                if(!visited[i][j] && map[i][j] == 1) {
+                    BFS(i, j);
+                    cnt++;
                 }
             }
         }
 
-        cout << answer << "\n";
-
-        cin >> W >> H;
+        cout << cnt << '\n';
+        cin >> N >> M;
     }
 
     return 0;
