294bced550f4c2789a257a81b1a6122a7fbff871
동재 <59719500+jea0716@users.noreply.github.com>
2025-01-26 16:25:02 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    if angle > 0 && angle < 90 {
+        return 1
+    } else if angle == 90 {
+        return 2
+    } else if angle < 180 {
+        return 3
+    } else {
+        return 4
+    }
+}
